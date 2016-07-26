8c7a21e9571fa2e2d70824c8ed1b444d3d4ca53b
Team Directory Matthew Brozen <matthewbrozen@Matthews-MacBook-Pro.local>
2016-07-25 23:54:08 -0700

    initial commit alpha

---
+++ main.js
@@ -0,0 +1,34 @@
+// console.log("hello world");
+
+// bluedream
+var bd =[10, 11, 12, 14, 15, 16, 17, 18, 19, 20]
+
+document.getElementById("percentage1").addEventListener("click", function(){
+  document.getElementById("displayPrice").innerHTML = "Blue Dream 19% $"+bd[0]+".00";})
+
+  document.getElementById("percentage2").addEventListener("click", function(){
+    document.getElementById("displayPrice").innerHTML = "Blue Dream 20% $"+bd[1]+".00";})
+
+    document.getElementById("percentage3").addEventListener("click", function(){
+      document.getElementById("displayPrice").innerHTML = "Blue Dream 21% $"+bd[2]+".00";})
+
+      document.getElementById("percentage4").addEventListener("click", function(){
+        document.getElementById("displayPrice").innerHTML = "Blue Dream 22% $"+bd[3]+".00";})
+
+        document.getElementById("percentage5").addEventListener("click", function(){
+          document.getElementById("displayPrice").innerHTML = "Blue Dream 24% $"+bd[4]+".00";})
+          //
+          // document.getElementById("six").addEventListener("click", function(){
+          //   document.getElementById("displayPrice").innerHTML = "Blue Dream 26% $"+bd[5]+".00";})
+          //
+          //   document.getElementById("percentage7").addEventListener("click", function(){
+          //     document.getElementById("displayPrice").innerHTML = "Blue Dream 27% $"+bd[6]+".00";})
+          //
+          //     document.getElementById("percentage8").addEventListener("click", function(){
+          //       document.getElementById("displayPrice").innerHTML = "Blue Dream 28% $"+bd[7]+".00";})
+          //
+          //       document.getElementById("percentage9").addEventListener("click", function(){
+          //         document.getElementById("displayPrice").innerHTML = "Blue Dream 29% $"+bd[8]+".00";})
+          //
+          //         document.getElementById("percentage10").addEventListener("click", function(){
+          //           document.getElementById("displayPrice").innerHTML = "Blue Dream 30 % $"+bd[9]+".00";})
---
+++ readme.md
@@ -0,0 +1,3 @@
+this is a price index for the nine most popular strains of cannabis in america,  their average price per gram, the price per gram at the most common THC content, the average price of each strain in a graph and a trend graph of the average price of that strain over the last month.
+
+the technologies uses are html, css, js
